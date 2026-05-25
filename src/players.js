38b07ff6cf8febbd8e99e726fef13ac8c8b1d9c8
RamahB0 <157064060+RamahB0@users.noreply.github.com>
2026-05-25 21:26:37 +0100

    Add players data to players.js

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami CF",
+    nationality: "Argentine",
+    jerseyNumber: 10,
+    age: 37,
+    image: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 39,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"
+  },
+  {
+    name: "Kylian Mbappe",
+    team: "Real Madrid",
+    nationality: "French",
+    jerseyNumber: 9,
+    age: 25,
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/57/2019-07-17_SJ-Sierra2_%28cropped%29.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norwegian",
+    jerseyNumber: 9,
+    age: 23,
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/67/Erling_Haaland_2023_%28cropped%29.jpg"
+  }
+];
+
+export default players;
